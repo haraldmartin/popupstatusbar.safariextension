177248227b655fe227d41e71525a19e66d07ce24
Maxwell Terpstra <telic.detour+git@gmail.com>
2010-06-10 18:32:39 -0700

    add ducking, remove flicker, make clickable
    Instead of hiding the statusdiv when hovered, just move it out of the way.
    Delay 2ms before hiding to give time for other mouseover events to fire (resolves flicker).
    Clicking the statusdiv now follows the link (in case it ever overlaps)
    Rerranged CSS rules into an order that makes more sense to me.

--- statusbar.js
+++ statusbar.js
@@ -1,26 +1,42 @@
-var StatusBarDiv = document.createElement("div");
+var StatusBarDiv = document.createElement("a");
 StatusBarDiv.className = "com_jev_statusbar-StatusBarDiv";
 document.body.appendChild(StatusBarDiv);
 
-var onTheDiv = false;
+var hideTimeout;
+
 StatusBarDiv.addEventListener("mouseover",
-function() {
-    onTheDiv = true
-},
-true);
+	function() {
+		clearTimeout(hideTimeout);
+		if (StatusBarDiv.style.bottom == "" || StatusBarDiv.style.bottom === "0px") {
+			StatusBarDiv.style.bottom = "20px"; // height + padding-top/bottom + 1
+			StatusBarDiv.style.borderBottomRightRadius = "5px";
+		} else {
+			StatusBarDiv.style.bottom = "0px";
+			StatusBarDiv.style.borderBottomRightRadius = "0";
+		}
+	},
+	true);
+StatusBarDiv.addEventListener("mouseout",
+	function() {
+		hideTimeout = setTimeout(hideStatusDiv, 2);
+	});
+
+function hideStatusDiv(event) {
+	StatusBarDiv.style.display = "none";
+	StatusBarDiv.style.bottom = 0;
+	StatusBarDiv.style.borderBottomRightRadius = "0";
+}
 
 function mouseover(event) {
-    if (!onTheDiv) {
-        StatusBarDiv.innerHTML = event.currentTarget.href;
-        StatusBarDiv.style.display = "block"
-    }
+	clearTimeout(hideTimeout);
+    StatusBarDiv.innerHTML = event.currentTarget.href;
+	StatusBarDiv.href = event.currentTarget.href;
+    StatusBarDiv.style.display = "block";
 }
 function mouseout(event) {
-    StatusBarDiv.innerHTML = "";
-    StatusBarDiv.style.display = "none";
-    onTheDiv = false
+	hideTimeout = setTimeout(hideStatusDiv, 2);
 }
-for (var i in document.links) {
-    document.links[i].addEventListener("mouseover", mouseover, true);
-    document.links[i].addEventListener("mouseout", mouseout, true);
-}
+Array.prototype.forEach.call(document.links, function(link) {
+    link.addEventListener("mouseover", mouseover, true);
+    link.addEventListener("mouseout", mouseout, true);
+});
