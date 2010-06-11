d77f1c435fa8a2f1342c2c471de920f3b38792d5
Maxwell Terpstra <telic.detour+git@gmail.com>
2010-06-10 20:31:13 -0700

    code cleanup
    rearranged JS and added comments.

--- statusbar.js
+++ statusbar.js
@@ -2,29 +2,74 @@ const HIDDEN = "com_jev_statusbar-hide";
 const DUCKED = "com_jev_statusbar-duck";
 const SHOWN = "";
 
-var StatusBarDiv = document.createElement("a");
-StatusBarDiv.id = "com_jev_statusbar-StatusBarDiv";
-StatusBarDiv.className = HIDDEN;
-document.body.appendChild(StatusBarDiv);
+var StatusBarDiv; // the DOM element with status text
+var hideTimeout; // holds timer used to prevent StatusBarDiv from flickering on/off
 
-var hideTimeout;
+/** Event Handler Functions **/
+/*****************************/
+
+/*
+ * hideStatusDiv
+ *  Hides the StatusBarDiv.  Should normally be called with a delay (using
+ *  setTimeout) to avoid flicker from other event handlers immidiately re-showing it.
+ */
+function hideStatusDiv(event) {
+	StatusBarDiv.className = HIDDEN;
+}
+
+/*
+ * linkHover
+ *  Updates the StatusBarDiv with the href of the currently hovered link.  Attached
+ *  as the "mouseover" event of all links on the page.
+ */
+function linkHover(event) {
+	clearTimeout(hideTimeout); // cancel delayed hideStatusDiv()
+    StatusBarDiv.innerHTML = event.currentTarget.href;
+	StatusBarDiv.href = event.currentTarget.href;
+    if (StatusBarDiv.className == HIDDEN) StatusBarDiv.className = SHOWN;
+	 // note: if StatusBarDiv.className == DUCKED, it is not hidden
+}
+
+/*
+ * linkUnHover
+ *  Queues hiding of the StatusBarDiv.  Attached as the "mouseout" event of all links
+ *  on the page.
+ */
+function linkUnHover(event) {
+	hideTimeout = setTimeout(hideStatusDiv, 2);
+}
+
+/*
+ * checkDuck
+ *  Used to "unduck" the StatusBarDiv when the mouse moves beyond its right edge.
+ *  Vertical movement doesn't need to be checked here, since this is already handled
+ *  sufficiently by mouseover and mouseout events elsewhere.
+ */
 function checkDuck(mouseEvent) {
-	console.log("checkduck");
 	if (mouseEvent.clientX > StatusBarDiv.offsetWidth) {
-		// unduck
 		StatusBarDiv.className = SHOWN;
 		window.removeEventListener("mousemove", checkDuck, true);
 	}
 }
 
+
+/**  Initialization **/
+/*********************/
+
+var StatusBarDiv = document.createElement("a");
+StatusBarDiv.id = "com_jev_statusbar-StatusBarDiv";
+StatusBarDiv.className = HIDDEN;
+document.body.appendChild(StatusBarDiv);
+
 StatusBarDiv.addEventListener(
 	"mouseover",
 	function() {
-		clearTimeout(hideTimeout);
-		if (StatusBarDiv.className == SHOWN || StatusBarDiv.className == HIDDEN) {
+		clearTimeout(hideTimeout); // don't hide while it is being hovered
+		if (StatusBarDiv.className != DUCKED) {
 			StatusBarDiv.className = DUCKED;
 			window.addEventListener("mousemove", checkDuck, true);
 		} else {
+			// mouse is crossing from bottom edge while ducked; unduck
 			StatusBarDiv.className = SHOWN;
 			window.removeEventListener("mousemove", checkDuck, true);
 		}
@@ -34,25 +79,14 @@ StatusBarDiv.addEventListener(
 StatusBarDiv.addEventListener(
 	"mouseout",
 	function() {
+		// hide unless linkHover event happens again immediately
 		hideTimeout = setTimeout(hideStatusDiv, 2);
 	},
 	true
 );
 
-function hideStatusDiv(event) {
-	StatusBarDiv.className = HIDDEN;
-}
-
-function mouseover(event) {
-	clearTimeout(hideTimeout);
-    StatusBarDiv.innerHTML = event.currentTarget.href;
-	StatusBarDiv.href = event.currentTarget.href;
-    if (StatusBarDiv.className != DUCKED) StatusBarDiv.className = SHOWN;
-}
-function mouseout(event) {
-	hideTimeout = setTimeout(hideStatusDiv, 2);
-}
+// attach event listeners to all existing links
 Array.prototype.forEach.call(document.links, function(link) {
-    link.addEventListener("mouseover", mouseover, true);
-    link.addEventListener("mouseout", mouseout, true);
+    link.addEventListener("mouseover", linkHover, true);
+    link.addEventListener("mouseout", linkUnHover, true);
 });
